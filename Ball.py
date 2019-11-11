911e0d105996ca32da7876cc22459dbe56b72cee
Adam Byron <abyron@nevada.unr.edu>
2019-11-10 19:39:17 -0800

    Added STOP and GO functionality (space)

--- Ball.py
+++ Ball.py
@@ -6,6 +6,9 @@ size = width, height = 320, 240
 speed = [2, 2]
 black = 0, 0, 0
 
+# track if ball is stopped
+stopped = 0
+
 screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("ball.png")
@@ -13,7 +16,20 @@ ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+        pressed = pygame.key.get_pressed()
+        # if 'w' key is pressed
+        if pressed[pygame.K_SPACE]:
+            if stopped == 1:
+                print("GO")
+                stopped = 0
+                speed = [2, 2]
+            elif stopped == 0:
+                print("STOP")
+                stopped = 1
+                speed = [0, 0]
+
 
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
